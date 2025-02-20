6efc555a93d4a783966616197638f5da50411fb6
Saviam <saviambruno@gmail.com>
2025-02-20 15:37:49 -0300

    requisição de api e estilização de pagina

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 
-import React from "react";
-
+import React, {useEffect, useState} from "react";
+import './style.css';
 ////https://sujeitoprogramador.com/rn-api/?api=posts
 
+
+
 function App() {
+  const [nutri, setNutri] = useState([]);
+
+  useEffect(()=>{
+
+    function loadApi(){
+      let url = 'https://sujeitoprogramador.com/rn-api/?api=posts'
+      
+      fetch(url)
+
+      .then((r)=> r.json())
+      .then((json)=>{
+        console.log(json);
+        setNutri(json);
+      })
+    }
+
+    loadApi();
+
+  },[]);
+
+
+
   return (
-    <div >
-     <h1>Teste</h1>
+    <div className="container" >
+      <header>
+        <strong>ReactNutri</strong>
+      </header>
+     {nutri.map((item)=>{
+      return(
+        <article key={item.id} className="post">
+          <strong className="titulo">{item.titulo}</strong>
+          <img src={item.capa} alt={item.titulo} className="capa"/>
+          <p className="subtitulo"> 
+            {item.subtitulo}
+          </p>
+          <a href="https://ge.globo.com/" className="botao">Acessar</a>
+        </article>
+      )
+     })}
     </div>
   );
 }
