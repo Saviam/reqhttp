152fadb20236c6a505bc31c3521ff0981edf7030
Saviam <saviambruno@gmail.com>
2025-02-20 14:55:16 -0300

    requisição

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+////https://sujeitoprogramador.com/rn-api/?api=posts
+
+function App() {
+  return (
+    <div >
+     <h1>Teste</h1>
+    </div>
+  );
+}
+
+export default App;
